d4bd21e24643c4524c00d580f549d27e5f1c23d3
Shrivu Shankar <shrivu1122@gmail.com>
2023-12-08 03:30:21 +0000

    whisper

--- multi_token/modalities/__init__.py
+++ multi_token/modalities/__init__.py
@@ -4,12 +4,14 @@ from multi_token.modalities.vision_clip import (
 )
 from multi_token.modalities.imagebind import ImageBindModality
 from multi_token.modalities.document_gte import DocumentGPTModality
+from multi_token.modalities.audio_whisper import WhisperAudioModality
 
 MODALITY_BUILDERS = {
     "vision_clip": lambda: [CLIPVisionModality()],
     "vision_clip_pool": lambda: [
         CLIPVisionModality(feature_layer=CLIP_PATCH_LAYER, num_tokens_output=10)
     ],
+    "audio_whisper": lambda: [WhisperAudioModality(num_tokens_output=10)],
     "imagebind": lambda: [ImageBindModality()],
     "document_gte": lambda: [DocumentGPTModality()],
     "document_gte_x16": lambda: [DocumentGPTModality(num_tokens_output=32)],
---
+++ scripts/whisper_build_pretrain_dataset.py
@@ -0,0 +1,80 @@
+from typing import List
+import random
+import argparse
+
+from datasets import load_dataset
+from datasets import Dataset
+
+from multi_token.constants import ROLE_ASSISTANT, ROLE_USER
+
+PRETRAIN_PHRASES = [
+    "Repeat the content of the audio <speech>",
+    "Transcribe <speech>",
+    "What is being said in <speech>",
+    "Can you interpret <speech>?",
+    "Please convert <speech> into text",
+    "What does <speech> say?",
+    "Could you transcribe <speech> for me?",
+    "I need the text of <speech>",
+    "Can you write out <speech>?",
+    "What's the content of <speech>?",
+    "Please provide the transcript of <speech>",
+    "Can you decode <speech>?",
+    "What is the transcription of <speech>?",
+    "Can you jot down <speech>?",
+    "What is the written form of <speech>?",
+    "Can you scribe <speech>?",
+]
+
+
+def _write_convo(row) -> List:
+    audio = dict(row["audio"])
+    audio.pop("path")
+    example = {
+        "speech_audios": [audio],
+    }
+    phrase = random.choice(PRETRAIN_PHRASES)
+    example["messages"] = [
+        {
+            "role": ROLE_USER,
+            "content": phrase,
+        },
+        {
+            "role": ROLE_ASSISTANT,
+            "content": row["text"] if "text" in row else row["sentence"],
+        },
+    ]
+    return example
+
+
+def main(args):
+    audio_dataset = load_dataset(
+        "mozilla-foundation/common_voice_11_0", "en", split="train"
+    )
+
+    idxs = list(range(len(audio_dataset)))
+    random.shuffle(idxs)
+
+    def gen():
+        i = 0
+        for idx in idxs:
+            row = audio_dataset[idx]
+            try:
+                yield _write_convo(row)
+            except ValueError:
+                pass
+            else:
+                i += 1
+                if i >= args.max_examples:
+                    break
+
+    ds = Dataset.from_generator(gen)
+    ds.save_to_disk(args.output_folder)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output_folder", type=str)
+    parser.add_argument("-n", "--max_examples", type=int, default=1_000_000)
+    args = parser.parse_args()
+    main(args)
